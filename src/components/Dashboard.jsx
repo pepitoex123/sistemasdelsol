23eb030a9d73fca6ac897c19a2345f182de6449b
Uriel Cabrera <urimartincabrera123@gmail.com>
2021-11-02 18:49:35 -0300

    basic header

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -4,6 +4,7 @@ const Dashboard = () => {
     return(
         <div className="dashboard">
             <Header/>
+
         </div>
     )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,18 @@
 import Logo from "./../media/headerlogo.png";
+import {BiUserCircle,BiStore,BiCart} from "react-icons/bi";
 
 
 const Header = () => {
     return(
-        <header>
-            <img src={Logo} alt=""/>
+        <header className="header">
+            <img src={Logo} alt="Droguería Sistemas Del Sol" className="header_logo"/>
+            <nav>
+                <ul>
+                    <li><BiCart/>Cart</li>
+                    <li><BiUserCircle/>Account</li>
+                    <li><BiStore/>Store</li>
+                </ul>
+            </nav>
         </header>
     )
 }
