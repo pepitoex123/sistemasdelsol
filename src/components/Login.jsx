9c8959bd57b55718384cf2b2b46b5ddf42ecdb16
Uriel Cabrera <urimartincabrera123@gmail.com>
2021-11-02 15:38:33 -0300

    added login component, initial setup with SCSS

---
+++ src/components/Login.jsx
@@ -0,0 +1,23 @@
+import Logo from "./../media/logo.png";
+import {BiUser,BiKey} from "react-icons/bi";
+
+const Login = () => {
+    return(
+        <section className="login">
+            <img src={Logo} className="login_image" alt="Drogueria Del Sol"/>
+            <h1 className="login_title">Login</h1>
+            <form className="login_form">
+                <BiUser/>
+                <label htmlFor="email">Email</label>
+                <input type="email" className="login_form_input" name="email" id="email" placeholder="Email"/>
+                <BiKey/>
+                <label htmlFor="password">Password</label>
+                <input type="password" className="login_form_input" name="password" id="password" placeholder="Password"/>
+                <input type="submit" value="Login" className="login_form_submit"/>
+            </form>
+        </section>
+    )
+}
+
+
+export default Login;
