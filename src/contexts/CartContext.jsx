f6df1e51c454c6817dc40db3ba6399ce27e9c0d0
Uriel Cabrera <urimartincabrera123@gmail.com>
2021-12-02 19:38:44 -0300

    added CartContext and CounterContext

---
+++ src/contexts/CartContext.jsx
@@ -0,0 +1,68 @@
+
+import {createContext} from "React";
+import {CounterContext} from "./CounterContext";
+
+
+export const CartContext = createContext();
+
+export const CartProvider = ({children}) => {
+    const [cart,setCart] = useState([]);
+
+    const {counter,setCounter} = useContext(CounterContext)
+
+    // I'm going to add different methods that will be definitely useful for interacting with the cart :D
+
+    // Voy a añadir diferentes métodos que definitivamente serán útiles para la interacción con el carrito ^-^
+
+
+    const clearCart = () => {
+        setCounter(0);
+        setCart([]);
+    }
+
+
+    const addProductToCart = (product,quantity) => {
+        let newCart = cart.map((prod) => {
+            if (product.id === prod.id) {
+                prod.quantity += quantity;
+            }
+            console.log(newCart)
+            return prod;
+        });
+        if (!newCart.some((prod) => prod.id === product.id)) {
+            newCart.push({
+                ...product,
+                quantity: quantity
+            });
+            console.log(newCart)
+        }
+        setCart(newCart);
+    }
+
+    const deleteProductFromCart = (id,quantity) => {
+        setCart(cart.filter(prod => prod.id !== id))
+        setCounter(amountOfProductsCart() - quantity)
+    }
+
+    const amountOfProductsCart = () => {
+        return cart.reduce((acc,prod) => acc + prod.quantity,0)
+    }
+
+    const getTotalPrice = () => {
+        let totalPrice = 0;
+
+        for(let product of cart){
+            totalPrice += product.totalForItem;
+        }
+        console.log(totalPrice)
+
+        return totalPrice;
+    }
+
+
+    return(
+        <CartContext.Provider value={{cart,setCart,addProductToCart,getTotalPrice,clearCart,deleteProductFromCart,amountOfProductsCart}}>
+            {children}
+        </CartContext.Provider>
+    );
+};
