958ef2393972c52028ba5719cbcfcafb1d31cd14
Uriel Cabrera <urimartincabrera123@gmail.com>
2021-12-04 19:01:00 -0300

    added changes to cartWidget

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -15,7 +15,7 @@ const CartWidget = () => {
         <Link to="/dashboard/cartcheckout" style={{ color: 'inherit', textDecoration: 'inherit'}}>
             <li className="cartwidget">
                 <BiCart/>Carrito
-                {cart.reduce((acc,prod) => acc + prod.quantity,0) !== 0 && (<div>cart.reduce((acc,prod) => acc + prod.quantity,0)</div>)}
+                {cart.reduce((acc,prod) => acc + prod.quantity,0) !== 0 && (<div>{cart.reduce((acc,prod) => acc + prod.quantity,0)}</div>)}
             </li>
         </Link>
     )
