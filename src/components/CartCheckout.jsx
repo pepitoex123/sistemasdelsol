320e5a9723bbe8e281d8b5d87f3924ca8983221c
Uriel Cabrera <urimartincabrera123@gmail.com>
2021-12-04 18:50:52 -0300

    design changes in responsive table

--- src/components/CartCheckout.jsx
+++ src/components/CartCheckout.jsx
@@ -7,7 +7,7 @@ const CartCheckout = () => {
 
     return(
         <div className="cartcheckout">
-            <h1 className="cartcheckout__title">Cart Checkout</h1>
+            <h1 className="cartcheckout__title">Su Pedido</h1>
             <CartCheckoutTable/>
         </div>
     )
